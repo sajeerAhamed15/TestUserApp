a5b7389dd2a33388f3970beb3f68b97b008d5b22
sajeerAhamed15 <ahamedsajeer.15@cse.mrt.ac.lk>
2021-05-21 09:07:28 +0530

    Populate DB

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,10 +4,8 @@ exports.findAllUsers = (req, res) => {
     User.getAll((err, document) => {
         if (err)
             if (err.status === 204) {
-                res.status(err.status)
                 res.render('error', { message: "No Content", error: { status: err.status } })
             } else {
-                res.status(err.status)
                 res.render('error', { message: "Something went wrong", error: err })
             }
         else
@@ -18,17 +16,14 @@ exports.findAllUsers = (req, res) => {
 exports.createUser = (req, res) => {
     var new_user = new User(req.body)
     if (new_user.userName == null) {
-        res.status(403)
         res.render('error', { message: "userName is required", error: { status: 403 } })        
     } else {
         User.getByUserName(new_user.userName, (err, existing_user) => {
             if (existing_user) {
-                res.status(403)
                 res.render('error', { message: "UserName already exists", error: { status: 409 } })
             } else {
                 User.createNew(new_user, (err, document) => {
                     if (err) {
-                        res.status(err.status)
                         res.render('error', { message: "Something went wrong", error: err })
                     } else
                         res.render('output', { response: JSON.stringify(document) })
@@ -42,10 +37,8 @@ exports.findUser = (req, res) => {
     User.getByUserName(req.params.userName, (err, document) => {
         if (err)
             if (err.status === 204) {
-                res.status(err.status)
                 res.render('error', { message: "No Such User", error: { status: err.status } })
             } else {
-                res.status(err.status)
                 res.render('error', { message: "Something went wrong", error: err })
             }
         else
@@ -57,10 +50,8 @@ exports.deleteUser = (req, res) => {
     User.deleteByUserName(req.params.userName, (err, document) => {
         if (err)
             if (err.status === 204) {
-                res.status(err.status)
                 res.render('error', { message: "No Such User", error: { status: err.status } })
             } else {
-                res.status(err.status)
                 res.render('error', { message: "Something went wrong", error: err })
             }
         else
@@ -72,7 +63,6 @@ exports.updateUser = (req, res) => {
     if (req.body.userName) {
         User.getByUserName(req.body.userName, (err, existing_user) => {
             if (existing_user) {
-                res.status(409)
                 res.render('error', { message: "UserName already exists", error: { status: 409 } })
             }
         })
@@ -81,10 +71,8 @@ exports.updateUser = (req, res) => {
     User.updateByUserName(req.params.userName, req.body, (err, document) => {
         if (err)
             if (err.status === 204) {
-                res.status(err.status)
                 res.render('error', { message: "No Such User", error: { status: err.status } })
             } else {
-                res.status(err.status)
                 res.render('error', { message: "Something went wrong", error: err })
             }
         else
